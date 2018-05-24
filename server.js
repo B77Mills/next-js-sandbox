1f6829d582d104c2aebbf25736f1474d1f17a066
Jacob Bare <jacob@limit0.io>
2018-05-24 10:19:39 -0500

    All the things

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const next = require('next');
 const path = require('path');
+const helmet = require('helmet');
+const robots = require('./src/server/robots');
+const sitemap = require('./src/server/sitemap');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({
@@ -12,6 +15,7 @@ const { PORT } = process.env;
 
 app.prepare().then(() => {
   const server = express();
+  server.use(helmet());
 
   server.get('/story/:id', (req, res) => {
     const { id } = req.params;
@@ -23,6 +27,18 @@ app.prepare().then(() => {
   server.get('/favicon.ico', (req, res) => {
     const file = path.join(__dirname, 'src/static/favicon.ico');
     app.serveStatic(req, res, file);
+  });
+
+  server.get('/robots.txt', (req, res) => {
+    const txt = robots(`http://localhost:${PORT}`);
+    res.set('Content-Type', 'text/plain');
+    res.send(txt);
+  });
+
+  server.get('/sitemap.xml', (req, res) => {
+    const xml = sitemap();
+    res.set('Content-Type', 'text/xml');
+    res.send(xml);
   });
 
   server.get('*', (req, res) => {
