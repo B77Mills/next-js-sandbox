e918253bb58eede93fe21f7b4ccd98f2d9e3f1a6
Jacob Bare <jacob@limit0.io>
2018-05-24 10:47:20 -0500

    Reorganize server files

--- server.js
+++ server.js
@@ -1,55 +1,10 @@
-const express = require('express');
 const next = require('next');
-const path = require('path');
-const helmet = require('helmet');
-const robots = require('./src/server/robots');
-const sitemap = require('./src/server/sitemap');
+const loadServer = require('./src/server');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({
-  dev,
-  dir: './src',
-});
-const handle = app.getRequestHandler();
-const { PORT } = process.env;
+const app = next({ dev, dir: './src' });
 
-app.prepare().then(() => {
-  const server = express();
-  server.use(helmet());
-
-  server.get('/story/:id', (req, res) => {
-    const { id } = req.params;
-    const actualPage = '/story';
-    const props = { id } ;
-    app.render(req, res, actualPage, props);
-  });
-
-  server.get('/favicon.ico', (req, res) => {
-    const file = path.join(__dirname, 'src/static/favicon.ico');
-    app.serveStatic(req, res, file);
-  });
-
-  server.get('/robots.txt', (req, res) => {
-    const txt = robots(`http://localhost:${PORT}`);
-    res.set('Content-Type', 'text/plain');
-    res.send(txt);
-  });
-
-  server.get('/sitemap.xml', (req, res) => {
-    const xml = sitemap();
-    res.set('Content-Type', 'text/xml');
-    res.send(xml);
-  });
-
-  server.get('*', (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${PORT}`);
-  });
-}).catch((ex) => {
+app.prepare().then(() => loadServer(app)).catch((ex) => {
   console.error(ex.stack);
   process.exit(1);
 });
---
+++ src/server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+const robots = require('./robots');
+const sitemap = require('./sitemap');
+
+const { PORT } = process.env;
+
+const server = express();
+server.use(helmet());
+
+module.exports = (next) => {
+  const handle = next.getRequestHandler();
+
+  server.get('/story/:id', (req, res) => {
+    const { id } = req.params;
+    const actualPage = '/story';
+    const props = { id } ;
+    next.render(req, res, actualPage, props);
+  });
+
+  server.get('/favicon.ico', (req, res) => {
+    const file = path.join(__dirname, 'src/static/favicon.ico');
+    next.serveStatic(req, res, file);
+  });
+
+  server.get('/robots.txt', (req, res) => {
+    const txt = robots(`http://localhost:${PORT}`);
+    res.set('Content-Type', 'text/plain');
+    res.send(txt);
+  });
+
+  server.get('/sitemap.xml', (req, res) => {
+    const xml = sitemap();
+    res.set('Content-Type', 'text/xml');
+    res.send(xml);
+  });
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+};
