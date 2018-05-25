40b17ee1c14cd32abdeac409d7a494ea19780aa9
Jacob Bare <jacob@limit0.io>
2018-05-25 11:12:51 -0500

    Update story page to query base

--- src/pages/story.jsx
+++ src/pages/story.jsx
@@ -1,40 +1,59 @@
 import React from 'react';
-import fetch from 'isomorphic-unfetch';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 import Layout from '../components/MyLayout';
+import withApollo from '../apollo/client';
 
-const Story = props => (
-  <Layout>
-    <Head>
-      <title>{props.show.name}</title>
-      <meta name="description" content={props.show.summary} />
-    </Head>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
-    <img alt={props.show.name} src={props.show.image.medium} />
-  </Layout>
-);
+const STORY = gql`
+  query Story($input: ModelIdInput!) {
+    story(input: $input) {
+      id
+      title
+      teaser
+      body
+    }
+  }
+`;
+
+const createMarkup = html => ({ __html: html });
+
+const Story = ({ id }) => {
+  const input = { id };
+  return (
+    <Layout>
+      <Query query={STORY} variables={{ input }}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p><strong>{error.message}</strong></p>;
+
+          const { story } = data;
+
+          return (
+            <main>
+              <Head>
+                <title>{story.title}</title>
+                <meta name="description" content={story.teaser} />
+              </Head>
+              <h1>{story.title}</h1>
+              {/* eslint-disable-next-line react/no-danger */}
+              <article dangerouslySetInnerHTML={createMarkup(story.body)} />
+            </main>
+          );
+        }}
+      </Query>
+    </Layout>
+  );
+};
 
 Story.getInitialProps = async (context) => {
   const { id } = context.query;
-  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
-  const show = await res.json();
-
-  // eslint-disable-next-line no-console
-  console.log(`Fetched show: ${show.name}`);
-
-  return { show };
+  return { id };
 };
 
 Story.propTypes = {
-  show: PropTypes.shape({
-    name: PropTypes.string,
-    summary: PropTypes.string,
-    image: PropTypes.shape({
-      medium: PropTypes.string,
-    }),
-  }).isRequired,
+  id: PropTypes.string.isRequired,
 };
 
-export default Story;
+export default withApollo(Story);
