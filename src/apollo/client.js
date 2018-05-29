1ce27391f2503d3392dab0c75edc82f8d1b634d0
Jacob Bare <jacob@limit0.io>
2018-05-29 10:38:19 -0500

    Add onError logging

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,4 +1,6 @@
 import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
 import fetch from 'isomorphic-unfetch';
 import withApollo from './WithApollo';
 
@@ -9,11 +11,21 @@ const headers = {
 };
 
 const config = {
-  link: new HttpLink({
-    uri: 'https://base4.as3.io/graph',
-    headers,
-    fetch,
-  }),
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        // eslint-disable-next-line no-console
+        graphQLErrors.map(({ message, locations, path }) => console.error(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
+      }
+      // eslint-disable-next-line no-console
+      if (networkError) console.error(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: 'https://base4.as3.io/graph',
+      headers,
+      fetch,
+    }),
+  ]),
 };
 
 export default withApollo(config);
